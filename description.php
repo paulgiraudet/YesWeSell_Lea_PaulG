125be5d3edaaf6bb687a0f832bdbd12f1224789a
Tictac <paul.giraudet@gmail.com>
2018-09-10 15:06:12 +0200

    personnal page for each product array

--- description.php
+++ description.php
@@ -1,64 +1,28 @@
 <?php
-
-$products = array(
-  array('name' => 'Chaussures Checkerboard Classic Slip-On', 'description' => "Chaussures basses sans lacets, les Classic Slip-On sont dotées de finitions élastiques sur les côtés et d'un col rembourré pour plus de confort.", 'prix' => '€ 65,00', 'picture' => '1_blanc'),
-  array('name' => 'Chaussures en daim Authentic', 'description' => "Chaussures basses à lacets, les Authentic en daim de Vans arborent des coutures classiques et l'étiquette de la marque. Elles reposent sur une semelle extérieure gaufrée pour une adhérence accrue.", 'prix' => '€ 80,00', 'picture' => '2_bleu'),
-  array('name' => 'Chaussures Sk8-Hi MTE', 'description' => "Sa semelle vulcanisée crantée offre une adhérence optimale tandis que son bout renforcé résiste à l'usure. Un col rembourré vient aussi offrir davantage de confort.", 'prix' => '€ 110,00', 'picture' => '3_beige'),
-  array('name' => 'Chaussures AVE Rapidweld Pro Lite', 'description' => "Outre sa tige en synthétique, cette chaussure légère intègre une membrane Rapidweld sans coutures équipée de renforts DURACAP™ sur les zones les plus exposées à l'usure pour la durabilité et une semelle intérieure UltraCush Lite™ 3D pour plus de confort.", 'prix' => '€ 110,00', 'picture' => '4_blanc')
-);
-
-$productSelect = $_GET['productSelect'];
+$title=$_POST['name'];
+include('header.php');
 ?>
-<!doctype html>
-<html class="no-js" lang="fr">
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
-</head>
-
-<body>
-
-  <header>
-    <nav class="d-flex justify-content-between p-2 mx-4">
-      <h1><span class="colorTouch">Y</span>es<span class="colorTouch">W</span>e<span class="colorTouch">S</span>ell</h1>
-      <a href="index.html">Accueil</a>
-    </nav>
-    <!-- <div class="disclaimer">
-      <p class="text-center"><i>Ce site est juste un site vitrine, veuillez suivre nos réseaux sociaux pour un achat.</i></p>
-    </div> -->
-  </header>
 
   <nav aria-label="breadcrumb" class="mt-5">
     <ol class="breadcrumb mt-5">
-      <li class="breadcrumb-item mt-5"><a href="index.html">Accueil</a></li>
-      <li class="breadcrumb-item active mt-5" aria-current="page">Chaussures AVE Rapidweld Pro Lite</li>
+      <li class="breadcrumb-item mt-5"><a href="index.php">Accueil</a></li>
+      <li class="breadcrumb-item active mt-5" aria-current="page"><?php echo $_POST['name'] ?></li>
     </ol>
   </nav>
 
-  <div class="container mt-5 pt-5">
+  <div class="container mt-4">
 
     <div class="row p-0">
 
-
       <div class="col-12 col-md-6 mt-2 basketImage">
-        <img src="img/basket1_blanc.png" alt="first picture of basket" class="img-fluid">
+        <img src="img/<?php echo $_POST['picture'] ?>" alt="first picture of basket" class="img-fluid">
       </div>
 
       <div class="col-12 col-md-6 mt-2">
-        <h2 class="font-weight-bold mt-3"><?php echo $productSelect['name'];?></h2>
+        <h2 class="font-weight-bold mt-3"><?php echo $_POST['name'] ?></h2>
         <p class="description">Description</p>
-        <p class="aboutDescription">Outre sa tige en synthétique, cette chaussure légère intègre une membrane Rapidweld sans coutures équipée de renforts DURACAP™ sur les zones les plus exposées à l'usure pour la durabilité et une semelle intérieure UltraCush Lite™ 3D pour plus de confort.</p>
+        <p class="aboutDescription"><?php echo $_POST['description'] ?></p>
 
         <div class="productColor d-flex mt-4">
           <div class="firstColor clickActive d-flex justify-content-center align-items-center activeTouch">
@@ -93,14 +57,11 @@ $productSelect = $_GET['productSelect'];
           </div>
         </div>
 
-        <p class="description mt-2">€ 110,00</p>
+        <p class="description mt-2"><?php echo $_POST['price'] ?></p>
       </div>
 
     </div>
 
-    <?php echo 'img/basket' . $_GET['basketType'] . '.png'; ?>
-    <?php echo $_GET['test'];
-    echo $productSelect;?>
   </div>
     <script src="js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title> <?php echo $title; ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+  <header>
+    <nav class="d-flex justify-content-between p-2 mx-4">
+      <h1><span class="colorTouch">Y</span>es<span class="colorTouch">W</span>e<span class="colorTouch">S</span>ell</h1>
+      <a href="index.php">Accueil</a>
+    </nav>
+
+  </header>
